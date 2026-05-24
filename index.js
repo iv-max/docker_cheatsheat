58b354ce06286e2821ce503d45207a9c35c542f2
ivma87 <112741105+ivma87@users.noreply.github.com>
2026-05-23 18:06:57 -0700

    port fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const seedRoutes = require("./routes/seedRoutes");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
